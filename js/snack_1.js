37644f3644ad32b264262cbf31d3fce61f872c2f
emilianogastaldo <emiliano0303@gmail.com>
2024-01-16 18:05:50 +0100

    added guests and table const

---
+++ js/snack_1.js
@@ -0,0 +1,19 @@
+/*
+SNACK 1
+Il Grande Gatsby ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
+Ci ha lasciato il nome del tavolo  e la lista degli invitati in ordine di posto.
+const tableName = 'Tavolo Vip';
+*/
+
+const guests = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
