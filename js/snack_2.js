5ffc4fcd5718019e2d2bccea2d29952a196a086b
emilianogastaldo <emiliano0303@gmail.com>
2024-01-16 18:27:42 +0100

    created studentlist snack 2

---
+++ js/snack_2.js
@@ -0,0 +1,25 @@
+/*
+SNACK 2
+Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e 
+somma totale dei loro voti di esame...
+Per preparare l'aula di un nuovo corso, dobbiamo svolgere una serie di operazioni
+
+1. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 
+    e id superiore a 120
+3. Dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente 
+    il loro nome tutto in maiuscolo ES (Marco della Rovere => MARCO DELLA ROVERE);
+*/
+
+
+console.log('---- SNACK 2 ----')
+
+const studentsList = [
+    { id: 213, name: 'Marco della Rovere', grades: 78 },
+    { id: 110, name: 'Paola Cortellessa', grades: 96 },
+    { id: 250, name: 'Andrea Mantegna', grades: 48 },
+    { id: 145, name: 'Gaia Borromini', grades: 74 },
+    { id: 196, name: 'Luigi Grimaldello', grades: 68 },
+    { id: 102, name: 'Piero della Francesca', grades: 50 },
+    { id: 120, name: 'Francesca da Polenta', grades: 84 }
+];
