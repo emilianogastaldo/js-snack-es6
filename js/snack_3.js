ef4e74b5a7f1902bee308cfb66e40516e0cdd378
emilianogastaldo <emiliano0303@gmail.com>
2024-01-17 15:26:51 +0100

    scaffolding

---
+++ js/snack_3.js
@@ -0,0 +1,7 @@
+/*
+SNACK 3
+
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: name e peso.
+Stampare in console la bici con peso minore utilizzando il destructuring
+*/ 
